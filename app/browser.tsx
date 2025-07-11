a506a8c91d2b8208d162892023e652a44987749e
furium <shouldBnewB@gmail.com>
2025-07-11 15:37:25 +0900

    (add) 브라우저 스크린에 URL 표시 기능 추가 및 상태 관리 구현

--- app/browser.tsx
+++ app/browser.tsx
@@ -1,10 +1,21 @@
 import { useLocalSearchParams } from 'expo-router';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { useMemo, useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import WebView from 'react-native-webview';
 
 const styles = StyleSheet.create({
   safearea: {
     flex: 1,
+    backgroundColor: 'black',
+  },
+  urlContainer: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 5,
+  },
+  urlText: {
+    color: 'white',
   },
 });
 
@@ -12,9 +23,22 @@ const BrowserScreen = () => {
   // MEMO: 해당 페이지로 전달된 searchParams 읽어옴.
   const params = useLocalSearchParams();
   const initialUrl = params.initialUrl as string;
+
+  const [url, setUrl] = useState(initialUrl);
+  const urlTitle = useMemo(() => url.replace('https://', '').split('/')[0], [url]);
+
   return (
     <SafeAreaView style={styles.safearea}>
-      <WebView source={{ uri: initialUrl }} />
+      <View style={styles.urlContainer}>
+        <Text style={styles.urlText}>{urlTitle}</Text>
+      </View>
+      <WebView
+        source={{ uri: initialUrl }}
+        // MEMO: 현재 접속해있는 페이지 정보를 가져올 수 있음.
+        onNavigationStateChange={event => {
+          setUrl(event.url);
+        }}
+      />
     </SafeAreaView>
   );
 };
