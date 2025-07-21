aafca1acbaf6cf215f67214360af5a326bfb0445
furium <shouldBnewB@gmail.com>
2025-07-21 16:36:31 +0900

    (add) 쇼핑 스크린에  Pull To Refresh 구현

--- app/(tabs)/shopping.tsx
+++ app/(tabs)/shopping.tsx
@@ -1,22 +1,73 @@
 import { router } from 'expo-router';
-import { Text, TouchableOpacity, View } from 'react-native';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useCallback, useRef, useState } from 'react';
+import { RefreshControl, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  safearea: {
+    flex: 1,
+  },
+});
+
+const SHOPPING_HOME_URL = 'https://shopping.naver.com';
 
 const ShoppingScreen = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const webViewRef = useRef<WebView>(null);
+
+  const handleRefresh = useCallback(() => {
+    if (webViewRef.current) {
+      setRefreshing(true);
+      webViewRef.current.reload();
+    }
+  }, []);
   return (
-    <View>
-      <Text>Shopping</Text>
-      <TouchableOpacity
-        onPress={() => {
-          router.navigate({
-            pathname: 'browser',
-          });
+    <SafeAreaView style={styles.safearea}>
+      {/* ScrollView에는 2개의 스타일이 있음. */}
+      <ScrollView
+        // 컨테이너의 스타일
+        // style={{ backgroundColor: 'red' }}
+        // 컨테이너 안의 콘텐츠의 스타일
+        contentContainerStyle={{
+          flex: 1,
         }}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
       >
-        <Text>Go To Browser</Text>
-      </TouchableOpacity>
-      <MaterialCommunityIcons name="shopping" size={24} color="red" />
-    </View>
+        <WebView
+          ref={webViewRef}
+          source={{
+            uri: SHOPPING_HOME_URL,
+          }}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+          onShouldStartLoadWithRequest={request => {
+            // 네이버 쇼핑 도메인 내부 URL은 WebView에서 처리
+            if (request.url.startsWith(SHOPPING_HOME_URL) || request.mainDocumentURL?.startsWith(SHOPPING_HOME_URL))
+              return true;
+
+            // 외부 URL(https://)은 browser 화면으로 라우팅
+            if (request.url !== null && request.url.startsWith('https://')) {
+              router.navigate({
+                pathname: 'browser',
+                params: { initialUrl: request.url },
+              });
+              return false;
+            }
+
+            // 그 외 URL은 WebView에서 처리
+            return true;
+          }}
+          // 웹뷰 로딩이 끝나면 실행될 함수 (only 성공했을 때만.)
+          onLoad={() => {
+            setRefreshing(false);
+          }}
+          // 밑에 2개는 기본 로딩 UI를 빈 프라그먼트로 제거하는 props
+          renderLoading={() => <></>}
+          startInLoadingState={true}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+구현 목록
+
+1. 앱 네비게이션
+2. web view 구현
+3. 브라우저 기능 구현
+4. 라우팅 프로그레스 바 구현
+5. pull to refresh 구현
+
+- React Native의 ScrollView와 RefreshControl 컴포넌트 이용
+
+- ScrollView: 스크롤이 가능한 영역 제공
+  - https://reactnative.dev/docs/scrollview
+
+- RefreshControl: ScrollView의 새로고침 상태 표시
+  - https://reactnative.dev/docs/refreshcontrol
